5a531cebbea2f4cef8409210a74af3f9250b82fc
Maciej Zasada <maciejzsd@gmail.com>
2014-03-26 18:01:00 +0100

    working directory fix

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var path = require('path'),
     fs = require('fs'),
     argv = require('minimist')(process.argv.slice(2)),
-    longlang = require('./lib/longlang.js');
+    longlang = require('./lib/longlang.js'),
+    cwd = process.cwd();
 
 function usage () {
     console.log('Usage:', process.argv[0], process.argv[1], 'input.json [--scale=2] [--output=file.json]');
@@ -13,8 +14,8 @@ if (argv._.length !== 1) {
     return usage();
 }
 
-longlang(path.resolve(__dirname, argv._[0]), argv, function (result) {
-    var outputPath = path.resolve(__dirname, argv.output || 'longlang.json');
+longlang(path.resolve(cwd, argv._[0]), argv, function (result) {
+    var outputPath = path.resolve(cwd, argv.output || 'longlang.json');
     fs.writeFile(outputPath, JSON.stringify(result, null, 4), function (error) {
         if (error) {
             return console.log(error);
