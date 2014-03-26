84ee01e40f3f5b4933fb714b358803562695c30c
Maciej Zasada <maciejzsd@gmail.com>
2014-03-26 12:37:03 +0100

    v. 0.1.0

---
+++ index.js
@@ -0,0 +1,22 @@
+var path = require('path'),
+    fs = require('fs'),
+    argv = require('minimist')(process.argv.slice(2)),
+    longlang = require('./lib/longlang.js');
+
+function usage () {
+    console.log('Usage:', process.argv[0], process.argv[1], 'input.json [--scale=2] [--output=file.json]');
+}
+
+if (argv._.length !== 1) {
+    return usage();
+}
+
+longlang(path.resolve(__dirname, argv._[0]), argv, function (result) {
+    var outputPath = path.resolve(__dirname, argv.output || 'longlang.json');
+    fs.writeFile(outputPath, JSON.stringify(result, null, 4), function (error) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Output saved to', outputPath);
+    });
+});
